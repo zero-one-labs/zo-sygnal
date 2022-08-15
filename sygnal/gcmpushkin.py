62159ccf951849b71d4bfba378cecfa46bfccc2e
Chetan Khandla <ckhandla94@gmail.com>
2022-08-15 15:06:00 +0530

    Update gcmpushkin.py

--- sygnal/gcmpushkin.py
+++ sygnal/gcmpushkin.py
@@ -370,7 +370,7 @@ class GcmPushkin(ConcurrencyLimitedPushkin):
             body["data"] = data
             body["notification"] = {
                 "title": n.sender_display_name,
-                "body": "Open app to read messages"
+                "body": n.content.body
             }
             body["priority"] = "normal" if n.prio == "low" else "high"
 
